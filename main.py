39d7f2f0f654d33e0fde3a74f45083e4bd0538bf
cmd919293 <h94313@gmail.com>
2019-07-25 22:58:44 +0800

    update something

--- main.py
+++ main.py
@@ -2,43 +2,41 @@ import numpy as np
 import imgaug as ia
 from imgaug.augmentables.batches import UnnormalizedBatch
 from imgaug import augmenters as iaa
-import imageio , os , glob , sys , random
+import imageio
+import os
+import glob
+import sys
+import random
 
-def generateImage(loadPath,savePath):
- 
- fileName = glob.glob(loadPath)
- 
- if os.path.exists(savePath) == False:
-  os.mkdir(savePath)
-  
- images = []
- for name in fileName:
-  image = imageio.imread(name)
-  images.append(image)
+def generateImage(loadPath, savePath):
 
- batches = [UnnormalizedBatch(images=images)]
- mode = random.randint(0,100)
- aug = iaa.Sequential()
- mode = mode % 3
- 
- if mode == 0:
-  size = random.uniform(0.01,0.02) 
-  density = random.uniform(0.15,0.3) 
-  aug.add(iaa.CoarseSalt((0.1,density), size_percent=(0.008,size)))
- elif mode == 1:
-  col = random.randint(3,6)
-  row = random.randint(3,6)
-  replace = random.uniform(0.4,0.5) 
-  aug.add(iaa.PiecewiseAffine(scale=(0.02), nb_cols=(2,col), nb_rows=(2,row)))
-  aug.add(iaa.Superpixels(p_replace=(0.3,replace), n_segments=100) )	
-  
- batches_aug = list(aug.augment_batches(batches, background=False))
- 
- i = 0
- for name in fileName:
-  imageio.imwrite(os.path.join(savePath, os.path.basename(name)), np.uint8(batches_aug[0].images_aug[i]))
-  i = i + 1
-  
-  
+    fileName = glob.glob(loadPath)
+
+    if os.path.exists(savePath) == False:
+        os.makedirs(savePath)
+
+    images = list(map(lambda name: imageio.imread(name), fileName))
+
+    batches = UnnormalizedBatch(images=images)
+    mode = random.randint(0, 100) % 3
+    aug = iaa.Sequential()
+
+    if mode == 0:
+        size = random.uniform(0.01, 0.02)
+        density = random.uniform(0.15, 0.3)
+        aug.add(iaa.CoarseSalt((0.1, density), size_percent=(0.008, size)))
+    elif mode == 1:
+        col = random.randint(3, 6)
+        row = random.randint(3, 6)
+        replace = random.uniform(0.4, 0.5)
+        aug.add(iaa.PiecewiseAffine(scale=(0.02), nb_cols=(2, col), nb_rows=(2, row)))
+        aug.add(iaa.Superpixels(p_replace=(0.3, replace), n_segments=100))
+
+    batches_aug = list(aug.augment_batches(batches, background=False))[0]
+
+    for i, name in enumerate(fileName):
+        imageio.imwrite(os.path.join(savePath, os.path.basename(name)), np.uint8(batches_aug.images_aug[i]))
+
+
 if __name__ == '__main__':
- generateImage(sys.argv[1],sys.argv[2])
+    generateImage(sys.argv[1], sys.argv[2])
